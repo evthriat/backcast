2b792f54e00a0e2d5620a28de899554a346db04d
evthriat <evantatkinson@gmail.com>
2017-12-11 21:51:08 -0800

    added video thumbnails

--- src/views/app.js
+++ src/views/app.js
@@ -1,9 +1,19 @@
 var AppView = Backbone.View.extend({
+  
+    
 
   el: '#app',
 
   initialize: function() {
     this.videos = new Videos();
+    this.mainVideo = new Video(window.exampleVideoData[0]);
+
+    this.videoPlayer = new VideoPlayerView({model: this.mainVideo});
+    this.videoList = new VideoListView();
+    this.render().$el.find('.player h5').append(this.videoPlayer.$el);
+    this.$el.find('.list').append(this.videoList.$el);
+    
+
   },
 
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,6 +1,13 @@
 var VideoListView = Backbone.View.extend({
-
-
+  initialize: function() {
+    this.render();
+    var thisVideoList = this;
+    _.each(window.exampleVideoData, function(videoObj) {
+      var tempVideo = new Video(videoObj);
+      var tempVideoListEntery = new VideoListEntryView({model: tempVideo});
+      thisVideoList.$el.append(tempVideoListEntery.render().$el);
+    });
+  },
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
